f0020b9e936ab1eb9017facefad1044601b014dd
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-11 17:36:29 +0100

    add test for male age 40-49

--- spec/cooper-spec.js
+++ spec/cooper-spec.js
@@ -1,5 +1,9 @@
 describe("cooper", function(){
-  it("returns a correct data on calculation", function(){
+  it("returns a correct data on calculation for man 50+ age", function(){
     expect(cooper({age: 53, gender: "man", distance: 2000})).toEqual("Above Average");
   });
+
+  it("returns a correct data on calculation for man 40-49 age", function(){
+    expect(cooper({age: 47, gender: "man", distance: 1423})).toEqual("Bellow Average");
+  })
 })
