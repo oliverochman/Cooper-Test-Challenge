f9a02864b40acc828e67bbadf137526c31e675a4
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-11 17:44:09 +0100

    add spec for male 30-39 age

--- spec/cooper-spec.js
+++ spec/cooper-spec.js
@@ -6,4 +6,8 @@ describe("cooper", function(){
   it("returns a correct data on calculation for man 40-49 age", function(){
     expect(cooper({age: 47, gender: "man", distance: 1423})).toEqual("Bellow Average");
   })
+
+  it("returns a correct data on calculation for man 30-39 age", function(){
+    expect(cooper({age: 33, gender: "man", distance: 2000})).toEqual("Average");
+  })
 })
